a6c7b7eb5ec0928e40115827b729f972a5966d30
Elijah <2017008345@student.unza.zm>
2021-05-17 16:03:48 +0200

    a

--- routes/etds.js
+++ routes/etds.js
@@ -14,7 +14,7 @@ router.use(bodyParser.urlencoded({extended:true}))
 router.use(bodyParser.json())
 //Use methodOverride
 router.use(methodOverride(function(req, res){
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
         var method = req.body._method
         delete req.body._method
         return method
